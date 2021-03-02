1586be47043b859aa73d963bdb979b8dce8ad6cd
ziyi-bear <m0724001@gm.nuu.edu.tw>
2021-03-02 14:09:32 +0800

    clear and write demo data

---
+++ README.md
@@ -0,0 +1,27 @@
+[Learn VueJS by building a simple Todo-App](https://medium.com/@hugo.bjarred/learn-vuejs-by-building-a-simple-todo-app-44e2e7dfccae)
+
+## reference
+https://medium.com/@hugo.bjarred/learn-vuejs-by-building-a-simple-todo-app-44e2e7dfccae
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+// 初始Demo資料輸入
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'pfsense HAProxy',
+          complete: true
+        },
+        {
+          id: 2,
+          title: 'pfsense Suricata',
+          complete: true
+        },
+        {
+          id: 3,
+          title: 'pfsense OpenVPN',
+          complete: true
+        },
+        {
+          id: 4,
+          title: 'pfsense on Vmware',
+          complete: true
+        },
+        {
+          id: 5,
+          title: 'pfsense ACME',
+          complete: true
+        },
+        {
+          id: 6,
+          title: 'pfsense BIND DNS',
+          complete: false
+        }
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
